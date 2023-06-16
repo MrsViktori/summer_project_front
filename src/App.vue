28354a514b26300c97ed1e380423af5f67982820
Vika Yakovleva <vika.yakovleva.2003@yandex.ru>
2023-06-17 02:26:10 +0300

    Сделана часть сайта

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,100 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="project" >
+    <Advertisement text />
+    <Menu/>
+    <TitleVpnServer titleOne></TitleVpnServer>
+      <About></About>
+    <div class="blockClass">
+      <Blocks v-bind="cardsOne"></Blocks>
+      <Blocks v-bind="cardsTwo"></Blocks>
+      <Blocks v-bind="cardsThree"></Blocks>
+      <Blocks v-bind="cardsFour"></Blocks>
+      <Blocks v-bind="cardsFive"></Blocks>
+      <Blocks v-bind="cardsSix"></Blocks>
+      <Blocks v-bind="cardsSeven"></Blocks>
     </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
+
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script>
+
+import {defineComponent} from "vue";
+import Advertisement from "@/components/Header/Advertisement.vue";
+import Menu from "@/components/Header/Menu.vue";
+import TitleVpnServer from "@/components/Body/TitleVpnServer.vue";
+import About from "@/components/Body/About.vue";
+import Blocks from "@/components/Body/Blocks.vue";
+
+export default defineComponent({
+  components: {Blocks, About, TitleVpnServer, Menu, Advertisement},
+  data() {
+    return{
+      cardsOne:{
+        id: 1,
+        img: "../src/assets/body/about/processor.svg",
+        imgBlockOval: "../src/assets/body/about/blockOval.svg",
+        text: "Быстрые серверные процессоры INTEL® XEON® 2186g 3.8 GHz"
+      },
+      cardsTwo:{
+        id: 2,
+        img: "../src/assets/body/about/ssd.svg",
+        imgBlockOval: "../src/assets/body/about/blockOval.svg",
+        text: "Высокопроизводительные SSD-диски SAMSUNG и INTEL NVMe"
+      },
+      cardsThree:{
+        id: 3,
+        img: "../src/assets/body/about/os.svg",
+        imgBlockOval: "../src/assets/body/about/blockOval.svg",
+        text: "Поддержка разных ОС: CentOS, Debian, Ubuntu, Windows 2016r2"
+      },
+      cardsFour:{
+        id: 4,
+        img: "../src/assets/body/about/dataCenter.svg",
+        imgBlockOval: "../src/assets/body/about/blockOval.svg",
+        text: "Два независимых ЦОД. Добавьте еще текст."
+      },
+      cardsFive:{
+        id: 5,
+        img: "../src/assets/body/about/virtual.svg",
+        imgBlockOval: "../src/assets/body/about/blockOval.svg",
+        text: "Виртуализация VMWare\n" +
+            "Добавьте текст."
+      },
+      cardsSix:{
+        id: 6,
+        img: "../src/assets/body/about/copy.svg",
+        imgBlockOval: "../src/assets/body/about/blockOval.svg",
+        text: "Резервное копирование Veeam Backup"
+      },
+      cardsSeven:{
+        id: 7,
+        img: "../src/assets/body/about/channels.svg",
+        imgBlockOval: "../src/assets/body/about/blockOval.svg",
+        text: "Каналы 200 Гбит/c. Добавьте еще текст."
+      },
+    }
+  }
+})
+</script>
+
+<style>
+.project{
+  width: 100%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  flex-direction: column;
+}
+.blockClass{
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 1360px;
+  height: 461px;
+  margin-left: 120px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ src/components/Body/About.vue
@@ -0,0 +1,164 @@
+<template>
+  <div class="about">
+    <div class="sectionItemLeft">
+      <div class="aboutTitleOne">
+        <span class="textTitleOne">{{ titleOne }}</span>
+        <span class="textTitleTwo">{{ descriptionTitle }}</span>
+      </div>
+      <div class="aboutTitleTwo">
+        <span class="textTitleThree">{{ titleTwo }}</span>
+        <div class="list">
+          <div class="listOne">
+            <img :src="imgOval" class="listImgOval">
+            <span class="textListOne">{{ listOne }}</span>
+          </div>
+          <div class="listOne">
+            <img :src="imgOval" class="listImgOval">
+            <span class="textListOne">{{ listTwo }}</span>
+          </div>
+          <div class="listOne">
+            <img :src="imgOval" class="listImgOval">
+            <span class="textListOne">{{ listThree }}</span>
+          </div>
+          <div class="listOne">
+            <img :src="imgOval" class="listImgOval">
+            <span class="textListOne">{{ listFour }}</span>
+          </div>
+          <div class="listOne">
+            <img :src="imgOval" class="listImgOvalFive">
+            <span class="textListOne">{{ listFive }}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="sectionItemRight">
+      <img class="aboutImgServer" :src="imgServer">
+    </div>
+  </div>
+  <span class="sectionText">{{ titleThree }}</span>
+</template>
+
+<script>
+export default {
+  name: "About",
+  data() {
+    return {
+      titleOne: "Что такое виртуальный сервер?",
+      descriptionTitle: "Это физический сервер, разделенный с помощью виртуализации на несколько виртуальных серверов с гарантированным выделением ресурсов: оперативная память, процессор, IOPS. На каждый виртуальный сервер можно установить свою ОС: CentOS, Debian, Ubuntu, Windows.",
+      titleTwo: "Дополнительные преимущества:",
+      listOne: "Резервное копирование",
+      listTwo: "Отказоустойчивость SLA 99,95%",
+      listThree: "Безопасность",
+      listFour: "Высокая скорость",
+      listFive: "Удобный интерфейс управления",
+      imgServer: "../src/assets/body/about/server.svg",
+      imgOval: "../src/assets/body/about/oval.svg",
+      titleThree: "Программно-аппаратные возможности VPS",
+    }
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap');
+
+.about {
+  display: flex;
+  flex-direction: row;
+  border: 1px solid #D7DDEA;
+  border-radius: 4px;
+  margin-top: 77px;
+  width: 1120px;
+  height: 400px;
+  margin-left: 120px;
+}
+
+.aboutTitleOne {
+  margin-left: 32px;
+  margin-top: 31px;
+  height: 148px;
+  display: flex;
+  flex-direction: column;
+}
+
+.textTitleOne {
+  font-family: Montserrat;
+  font-weight: 700;
+  font-size: 26px;
+  line-height: 40px;
+  color: #373633;
+  width: 491px;
+  height: 40px;
+}
+
+.textTitleTwo {
+  width: 792px;
+  height: 84px;
+  margin-top: 24px;
+  font-family: 'Open Sans';
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 28px;
+  color: #747C8B;
+}
+
+.aboutTitleTwo {
+  margin-top: 48px;
+  margin-left: 32px;
+  display: flex;
+  flex-direction: column;
+  height: 140px;
+}
+
+.textTitleThree {
+  font-family: Montserrat;
+  font-weight: 700;
+  font-size: 22px;
+  line-height: 28px;
+}
+
+.list {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin-top: 32px;
+  width: 855px;
+  gap: 22px;
+}
+
+.listOne {
+  display: flex;
+  gap: 10px;
+  align-items: center;
+}
+
+.textListOne {
+  font-family: OpenSans;
+  font-size: 16px;
+  line-height: 24px;
+  color: #373633;
+  margin-right: 70px;
+  font-weight: 500;
+}
+
+.listImgOvalFive {
+  margin-left: 36px;
+}
+
+.sectionItemRight {
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-start;
+  margin-top: 53px;
+  margin-left: 100px;
+}
+.sectionText{
+  font-family: 'Montserrat';
+  font-weight: 700;
+  font-size: 28px;
+  line-height: 40px;
+  margin-left: 120px;
+  margin-top: 56px;
+}
+</style>
